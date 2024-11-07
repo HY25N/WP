9a29fc9e96dadd2091bd8ccc3ba36ac813263175
githings <b22042@naver.com>
2024-11-07 23:43:21 +0900

    기본 db 쿼리 구현
    System.Data.SQLite을 이용한 쿼리 구현

--- pr1/Form1.Designer.cs
+++ pr1/Form1.Designer.cs
@@ -81,9 +81,10 @@
             this.columnHeader9,
             this.columnHeader10});
             this.listView1.HideSelection = false;
-            this.listView1.Location = new System.Drawing.Point(190, 44);
+            this.listView1.Location = new System.Drawing.Point(271, 66);
+            this.listView1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.listView1.Name = "listView1";
-            this.listView1.Size = new System.Drawing.Size(598, 394);
+            this.listView1.Size = new System.Drawing.Size(853, 589);
             this.listView1.TabIndex = 17;
             this.listView1.UseCompatibleStateImageBehavior = false;
             this.listView1.View = System.Windows.Forms.View.Details;
@@ -163,206 +164,230 @@
             this.groupBox1.Controls.Add(this.label3);
             this.groupBox1.Controls.Add(this.label2);
             this.groupBox1.Controls.Add(this.label1);
-            this.groupBox1.Location = new System.Drawing.Point(12, 44);
+            this.groupBox1.Location = new System.Drawing.Point(17, 66);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(172, 252);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(246, 378);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "정보 입력";
             // 
             // numericUpDown5
             // 
-            this.numericUpDown5.Location = new System.Drawing.Point(65, 220);
+            this.numericUpDown5.Location = new System.Drawing.Point(93, 330);
+            this.numericUpDown5.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown5.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
             this.numericUpDown5.Name = "numericUpDown5";
-            this.numericUpDown5.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDown5.Size = new System.Drawing.Size(143, 28);
             this.numericUpDown5.TabIndex = 31;
             // 
             // numericUpDown3
             // 
-            this.numericUpDown3.Location = new System.Drawing.Point(65, 193);
+            this.numericUpDown3.Location = new System.Drawing.Point(93, 290);
+            this.numericUpDown3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown3.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
             this.numericUpDown3.Name = "numericUpDown3";
-            this.numericUpDown3.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDown3.Size = new System.Drawing.Size(143, 28);
             this.numericUpDown3.TabIndex = 30;
             // 
             // numericUpDown4
             // 
-            this.numericUpDown4.Location = new System.Drawing.Point(65, 166);
+            this.numericUpDown4.Location = new System.Drawing.Point(93, 249);
+            this.numericUpDown4.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown4.Maximum = new decimal(new int[] {
             20,
             0,
             0,
             0});
             this.numericUpDown4.Name = "numericUpDown4";
-            this.numericUpDown4.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDown4.Size = new System.Drawing.Size(143, 28);
             this.numericUpDown4.TabIndex = 29;
             // 
             // numericUpDown2
             // 
-            this.numericUpDown2.Location = new System.Drawing.Point(65, 139);
+            this.numericUpDown2.Location = new System.Drawing.Point(93, 208);
+            this.numericUpDown2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown2.Maximum = new decimal(new int[] {
             30,
             0,
             0,
             0});
             this.numericUpDown2.Name = "numericUpDown2";
-            this.numericUpDown2.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDown2.Size = new System.Drawing.Size(143, 28);
             this.numericUpDown2.TabIndex = 28;
             // 
             // numericUpDown1
             // 
-            this.numericUpDown1.Location = new System.Drawing.Point(65, 112);
+            this.numericUpDown1.Location = new System.Drawing.Point(93, 168);
+            this.numericUpDown1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.numericUpDown1.Maximum = new decimal(new int[] {
             30,
             0,
             0,
             0});
             this.numericUpDown1.Name = "numericUpDown1";
-            this.numericUpDown1.Size = new System.Drawing.Size(100, 21);
+            this.numericUpDown1.Size = new System.Drawing.Size(143, 28);
             this.numericUpDown1.TabIndex = 22;
             // 
             // textBox3
             // 
-            this.textBox3.Location = new System.Drawing.Point(65, 30);
+            this.textBox3.Location = new System.Drawing.Point(93, 45);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(100, 21);
+            this.textBox3.Size = new System.Drawing.Size(141, 28);
             this.textBox3.TabIndex = 27;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(65, 84);
+            this.textBox2.Location = new System.Drawing.Point(93, 126);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(100, 21);
+            this.textBox2.Size = new System.Drawing.Size(141, 28);
             this.textBox2.TabIndex = 26;
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(65, 57);
+            this.textBox1.Location = new System.Drawing.Point(93, 86);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 21);
+            this.textBox1.Size = new System.Drawing.Size(141, 28);
             this.textBox1.TabIndex = 25;
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(6, 222);
+            this.label8.Location = new System.Drawing.Point(9, 333);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(29, 12);
+            this.label8.Size = new System.Drawing.Size(44, 18);
             this.label8.TabIndex = 24;
             this.label8.Text = "기타";
             // 
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(6, 195);
+            this.label7.Location = new System.Drawing.Point(9, 292);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(29, 12);
+            this.label7.Size = new System.Drawing.Size(44, 18);
             this.label7.TabIndex = 23;
             this.label7.Text = "과제";
             // 
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(6, 168);
+            this.label6.Location = new System.Drawing.Point(9, 252);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(29, 12);
+            this.label6.Size = new System.Drawing.Size(44, 18);
             this.label6.TabIndex = 22;
             this.label6.Text = "출석";
             // 
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(6, 141);
+            this.label5.Location = new System.Drawing.Point(9, 212);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(29, 12);
+            this.label5.Size = new System.Drawing.Size(44, 18);
             this.label5.TabIndex = 21;
             this.label5.Text = "기말";
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(6, 114);
+            this.label4.Location = new System.Drawing.Point(9, 171);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(29, 12);
+            this.label4.Size = new System.Drawing.Size(44, 18);
             this.label4.TabIndex = 20;
             this.label4.Text = "중간";
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(6, 87);
+            this.label3.Location = new System.Drawing.Point(9, 130);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(41, 12);
+            this.label3.Size = new System.Drawing.Size(62, 18);
             this.label3.TabIndex = 19;
             this.label3.Text = "과목명";
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(6, 60);
+            this.label2.Location = new System.Drawing.Point(9, 90);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 12);
+            this.label2.Size = new System.Drawing.Size(44, 18);
             this.label2.TabIndex = 18;
             this.label2.Text = "이름";
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(6, 33);
+            this.label1.Location = new System.Drawing.Point(9, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(29, 12);
+            this.label1.Size = new System.Drawing.Size(44, 18);
             this.label1.TabIndex = 17;
             this.label1.Text = "학번";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(20, 310);
+            this.button1.Location = new System.Drawing.Point(29, 465);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(144, 23);
+            this.button1.Size = new System.Drawing.Size(206, 34);
             this.button1.TabIndex = 19;
             this.button1.Text = "입 력";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(20, 350);
+            this.button2.Location = new System.Drawing.Point(29, 525);
+            this.button2.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(144, 23);
+            this.button2.Size = new System.Drawing.Size(206, 34);
             this.button2.TabIndex = 20;
             this.button2.Text = "수 정";
             this.button2.UseVisualStyleBackColor = true;
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(20, 390);
+            this.button3.Location = new System.Drawing.Point(29, 585);
+            this.button3.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(144, 23);
+            this.button3.Size = new System.Drawing.Size(206, 34);
             this.button3.TabIndex = 21;
             this.button3.Text = "삭 제";
             this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 18F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.AutoSize = true;
-            this.ClientSize = new System.Drawing.Size(928, 518);
+            this.ClientSize = new System.Drawing.Size(1326, 777);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.listView1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form1";
             this.Text = "성적관리";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.groupBox1.ResumeLayout(false);
             this.groupBox1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.numericUpDown5)).EndInit();
--- pr1/Form1.cs
+++ pr1/Form1.cs
@@ -7,14 +7,128 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SQLite;
+using System.IO;
+using System.Data.SqlClient;
+using System.Runtime.InteropServices;
+using System.Xml.Linq;
+using System.Runtime.Remoting.Contexts;
 
 namespace pr1
 {
+
     public partial class Form1 : Form
     {
+        SQLiteConnection conn;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // todo 더 상위에서 초기화 해야할듯
+            InitDB("db");
+
+            UpdateListView();
+        }
+
+
+
+        private void InitDB(String dbName)
+        {
+
+            // db 생성 경로 지정
+            string exeDirPathString = Application.StartupPath;
+            exeDirPathString = Directory.GetParent(exeDirPathString).ToString();
+            exeDirPathString = Directory.GetParent(exeDirPathString).ToString();
+            var dbPath = $"{exeDirPathString}\\{dbName}.sqlite";
+
+            // db 파일 생성
+            if (!File.Exists(dbPath))
+            {
+                SQLiteConnection.CreateFile(dbPath);
+            }
+
+            // db 연결
+            string connectionString = $"Data Source={dbPath}; Version=3;";
+            conn = new SQLiteConnection(connectionString);
+            conn.Open();
+
+            // DB: DDL
+            string sql = "SELECT count(*) FROM sqlite_master WHERE name = 'student'";
+            int result = new SQLiteCommand(sql, conn).ExecuteNonQuery();
+
+
+            sql = "CREATE TABLE IF NOT EXISTS student (" +
+                "id int," +
+                "name varchar(50)," +
+                "lecture varchar(250)," +
+                "midTermScore int," +
+                "finalTermScore int)";
+            result = new SQLiteCommand(sql, conn).ExecuteNonQuery();
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            int id = int.Parse(textBox3.Text);
+            string name = textBox1.Text;
+            string lecture = textBox2.Text;
+            int midTermScore = (int)numericUpDown1.Value;
+            int finalTermScore = (int)numericUpDown2.Value;
+
+            string sql = $"insert into student (id, name, lecture, midTermScore, finalTermScore) values (" +
+                $"{id}, '{name}', '{lecture}', '{midTermScore}', '{finalTermScore}')";
+
+            int result = new SQLiteCommand(sql, conn).ExecuteNonQuery();
+
+            UpdateListView();
+        }
+
+
+
+        private int UpdateListView()
+        {
+            String sql;// = "insert into members (name, age) values ('한강', 46)";
+            int result; // = new SQLiteCommand(sql, conn).ExecuteNonQuery();
+
+            sql = "select * from student";
+            SQLiteDataReader sdr = new SQLiteCommand(sql, conn).ExecuteReader();
+
+            listView1.Items.Clear();
+            int cnt = 0;
+            while (sdr.Read())
+            {
+                ListViewItem lvi = new ListViewItem();
+                // lvi.Text = "er";
+                lvi.SubItems.Add(sdr["id"].ToString());
+                lvi.SubItems.Add(sdr["name"].ToString());
+                lvi.SubItems.Add(sdr["lecture"].ToString());
+                lvi.SubItems.Add(sdr["midTermScore"].ToString());
+                lvi.SubItems.Add(sdr["finalTermScore"].ToString());
+                //
+                listView1.Items.Add(lvi);
+                cnt++;
+            }
+
+            return cnt;
+        }
+
+        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            textBox2.Text = "1qq2"; //listView1.Items[listView1.FocusedItem.Index].Text;   
+        }
+
+        private void listView1_ItemCheck(object sender, ItemCheckEventArgs e)
+        {
+            textBox2.Text = "1123"; //listView1.Items[listView1.FocusedItem.Index].Text;   
+        }
+
+        private void listView1_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "112w"; //listView1.Items[listView1.FocusedItem.Index].Text;   
         }
     }
 }
