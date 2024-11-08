ab817d7a4c342571ec495e431bdd8a233610e519
githings <b22042@naver.com>
2024-11-08 13:49:10 +0900

    코드 정리

--- pr1/Form1.cs
+++ pr1/Form1.cs
@@ -12,7 +12,6 @@ using System.IO;
 using System.Data.Entity;
 using System.Diagnostics;
 
-
 namespace pr1
 {
     public class Student
@@ -29,6 +28,7 @@ namespace pr1
 
         public ApplicationDbContext() : base("DefaultConnection")
         {
+            // 파일 존재 여부는 여기서 처리해준다.
             Database.SetInitializer(new CreateDatabaseIfNotExists<ApplicationDbContext>());
 
         }
@@ -38,17 +38,46 @@ namespace pr1
     public partial class Form1 : Form
     {
         SQLiteConnection conn;
+        private ApplicationDbContext context;
 
 
         public Form1()
         {
             InitializeComponent();
+
+            Repository repo = new Repository();
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
-            // todo 더 상위에서 초기화 해야할듯
-            var context = new ApplicationDbContext();
+            InitDB("db");
+
+            // 데이터 추가
+            // context.Students.Add(new Student { Name = "Alice", Age = 22, Major = "Computer Science" });
+            // context.SaveChanges();
+
+            // 데이터 조회
+            // var students = context.Students.ToList();
+            // foreach (var student in students)
+            // {
+            //     Debug.WriteLine($"ID: {student.Id}, Name: {student.Name}, Age: {student.Age}, Major: {student.Major}");
+            // }
+
+            UpdateListView();
+        }
+
+
+
+        private void InitDB(String dbName)
+        {
+
+            // db 생성 경로 지정 (레거시이다. 이전 네이티브 방식에서 쓰던 것)
+            string exeDirPathString = Application.StartupPath;
+            exeDirPathString = Directory.GetParent(exeDirPathString).ToString();
+            exeDirPathString = Directory.GetParent(exeDirPathString).ToString();
+            var dbPath = $"{exeDirPathString}\\{dbName}.sqlite";
+
+            context = new ApplicationDbContext();
             context.Database.Initialize(force: true);
             // context.Database.Create();
 
@@ -61,61 +90,12 @@ namespace pr1
                     Major TEXT NOT NULL
                 )");
 
-            // 데이터 추가
-            context.Students.Add(new Student { Name = "Alice", Age = 21, Major = "Computer Science" });
-            context.SaveChanges();
-
-            // 데이터 조회
-            var students = context.Students.ToList();
-            foreach (var student in students)
-            {
-                Debug.WriteLine($"ID: {student.Id}, Name: {student.Name}, Age: {student.Age}, Major: {student.Major}");
-            }
-
-
-
-
-
-
-            InitDB("db");
-
-            UpdateListView();
-        }
-
-
-
-        private void InitDB(String dbName)
-        {
-
-            // db 생성 경로 지정
-            string exeDirPathString = Application.StartupPath;
-            exeDirPathString = Directory.GetParent(exeDirPathString).ToString();
-            exeDirPathString = Directory.GetParent(exeDirPathString).ToString();
-            var dbPath = $"{exeDirPathString}\\{dbName}.sqlite";
-
-            // db 파일 생성
-            if (!File.Exists(dbPath))
-            {
-                SQLiteConnection.CreateFile(dbPath);
-            }
-
-            // db 연결
-            string connectionString = $"Data Source={dbPath}; Version=3;";
-            conn = new SQLiteConnection(connectionString);
-            conn.Open();
-
-            // DB: DDL
-            string sql = "SELECT count(*) FROM sqlite_master WHERE name = 'student'";
-            int result = new SQLiteCommand(sql, conn).ExecuteNonQuery();
-
-
-            sql = "CREATE TABLE IF NOT EXISTS student (" +
-                "id int," +
-                "name varchar(50)," +
-                "lecture varchar(250)," +
-                "midTermScore int," +
-                "finalTermScore int)";
-            result = new SQLiteCommand(sql, conn).ExecuteNonQuery();
+            // 학번은 pk로 쓸 것이기 때문에 숫자타입으로 정의한다.
+            context.Database.ExecuteSqlCommand(@"
+                UPDATE sqlite_sequence 
+                SET seq=20240000
+                WHERE name ='Students'
+                ");
         }
 
         private void button1_Click_1(object sender, EventArgs e)
@@ -126,10 +106,15 @@ namespace pr1
             int midTermScore = (int)numericUpDown1.Value;
             int finalTermScore = (int)numericUpDown2.Value;
 
-            string sql = $"insert into student (id, name, lecture, midTermScore, finalTermScore) values (" +
-                $"{id}, '{name}', '{lecture}', '{midTermScore}', '{finalTermScore}')";
+            context.Students.Add(new Student
+            {
+                Id = id,
+                Name = name,
+                Age = midTermScore,
+                Major = finalTermScore.ToString()
+            });
 
-            int result = new SQLiteCommand(sql, conn).ExecuteNonQuery();
+            context.SaveChanges();
 
             UpdateListView();
         }
@@ -138,44 +123,23 @@ namespace pr1
 
         private int UpdateListView()
         {
-            String sql;// = "insert into members (name, age) values ('한강', 46)";
-            int result; // = new SQLiteCommand(sql, conn).ExecuteNonQuery();
-
-            sql = "select * from student";
-            SQLiteDataReader sdr = new SQLiteCommand(sql, conn).ExecuteReader();
-
             listView1.Items.Clear();
-            int cnt = 0;
-            while (sdr.Read())
+
+            var students = context.Students.ToList();
+            foreach (var student in students)
             {
                 ListViewItem lvi = new ListViewItem();
                 // lvi.Text = "er";
-                lvi.SubItems.Add(sdr["id"].ToString());
-                lvi.SubItems.Add(sdr["name"].ToString());
-                lvi.SubItems.Add(sdr["lecture"].ToString());
-                lvi.SubItems.Add(sdr["midTermScore"].ToString());
-                lvi.SubItems.Add(sdr["finalTermScore"].ToString());
+                lvi.SubItems.Add(student.Id.ToString());
+                lvi.SubItems.Add(student.Name.ToString());
+                lvi.SubItems.Add(student.Age.ToString());
+                lvi.SubItems.Add(student.Major.ToString());
                 //
                 listView1.Items.Add(lvi);
-                cnt++;
             }
 
-            return cnt;
+            return students.Count;
         }
 
-        private void listView1_SelectedIndexChanged(object sender, EventArgs e)
-        {
-            textBox2.Text = "1qq2"; //listView1.Items[listView1.FocusedItem.Index].Text;   
-        }
-
-        private void listView1_ItemCheck(object sender, ItemCheckEventArgs e)
-        {
-            textBox2.Text = "1123"; //listView1.Items[listView1.FocusedItem.Index].Text;   
-        }
-
-        private void listView1_Click(object sender, EventArgs e)
-        {
-            textBox2.Text = "112w"; //listView1.Items[listView1.FocusedItem.Index].Text;   
-        }
     }
 }
