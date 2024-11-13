629551b588f6a267edab9e340679f073aad61dfa
leeyoonhyo <yoonhyo0805@gmail.com>
2024-11-13 16:54:19 +0900

    Add files via upload

---
+++ WP1/WP1.Designer.cs
@@ -0,0 +1,149 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+using System;
+
+namespace WP1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(255, 46);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(285, 35);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "아이디를 입력해주세요";
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            this.textBox1.Leave += new System.EventHandler(this.textBox1_Leave);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(255, 163);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '*';
+            this.textBox2.Size = new System.Drawing.Size(285, 35);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "비밀번호를 입력해주세요";
+            this.textBox2.Enter += new System.EventHandler(this.textBox2_Enter);
+            this.textBox2.Leave += new System.EventHandler(this.textBox2_Leave);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("굴림체", 24F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(481, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(576, 74);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "원광대학교 학적관리";
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.Font = new System.Drawing.Font("맑은 고딕", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(42, 31);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 60);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "아이디\r\n";
+            // 
+            // label3
+            // 
+            this.label3.BackColor = System.Drawing.Color.White;
+            this.label3.Font = new System.Drawing.Font("맑은 고딕", 16.125F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(42, 151);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(198, 60);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "비밀번호";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.Location = new System.Drawing.Point(402, 240);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 41);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Location = new System.Drawing.Point(479, 256);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(563, 298);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+           
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(13F, 24F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1474, 829);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "메인";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private Button button1;
+        private GroupBox groupBox1;
+    }
+
+}
---
+++ WP1/WP1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace WP1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            textBox1.Enter += (sender, e) =>
+            {
+                if (textBox1.Text == "아이디를 입력해주세요")  // 기본 텍스트일 경우
+                {
+                    textBox1.Clear();  // 텍스트 지우기
+                    textBox1.ForeColor = Color.Black;  // 텍스트 색상 변경
+                }
+            };
+            textBox1.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox1.Text))
+                {
+                    textBox1.Text = "아이디를 입력해주세요";  // 기본 텍스트 복원
+                    textBox1.ForeColor = Color.Gray;  // 기본 색상 회색으로 복원
+                }
+
+
+            };
+            textBox2.Enter += (sender, e) =>
+            {
+                if (textBox2.Text == "비밀번호를 입력해주세요")  // 기본 텍스트일 경우
+                {
+                    textBox2.Clear();  // 텍스트 지우기
+                    textBox2.ForeColor = Color.Black;  // 텍스트 색상 변경
+                }
+            };
+            textBox2.Leave += (sender, e) =>
+            {
+                if (string.IsNullOrWhiteSpace(textBox2.Text))
+                {
+                    textBox2.Text = "비밀번호를 입력해주세요";  // 기본 텍스트 복원
+                    textBox2.ForeColor = Color.Gray;  // 기본 색상 회색으로 복원
+                }
+
+
+            };
+        }
+
+        private void textBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_Leave(object sender, EventArgs e)
+        {
+
+        }
+
+       
+    }
+}
+
